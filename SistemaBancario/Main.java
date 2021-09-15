02f3074de80946eae56f6c004ed27f89c0fd8eb0
May <dotta.may@gmail.com>
2021-09-15 14:16:24 -0300

    banco

---
+++ SistemaBancario/Main.java
@@ -0,0 +1,33 @@
+package SistemaBancario;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Utilizando a Entrada de Dados
+		ContaBanco cliente = new ContaBanco();
+		//cliente.EntradaDados();
+		
+		/*instanciando sem parâmetros
+		ContaBanco cliente1 = new ContaBanco();
+		cliente1.setNumAgencia("1234");		
+		cliente1.setNumConta("7788554");
+		cliente1.setBanco("Banco do Brasil");		
+		cliente1.abrirConta(1); //tipo: 1=corrente, 2=poupança; 3=conjunta, 4=conta encerrada
+		cliente1.setCliente("Maria");
+		cliente1.depositar(150);
+		cliente1.sacar(20);
+		cliente1.consultar();
+		cliente1.sacar(80);		
+		cliente1.fecharConta();		
+		cliente1.consultar();
+		
+		//instanciando com parâmetros
+		ContaBanco cliente2 = new ContaBanco("2222", "1122661", "Banco Inter", 2, "José", 0, true);	
+		cliente2.depositar(500);
+		cliente2.sacar(200);	
+		cliente2.consultar();
+		cliente2.sacar(100);			
+		cliente2.fecharConta();
+		cliente2.consultar();*/
+	}
+}
